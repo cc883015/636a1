1ebdbb954f932a93a0e817d498f116446babf67f
cc883015 <1044940343@qq.com>
2025-03-28 10:29:17 +1000

    update dependency and some codes

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Scheman({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
