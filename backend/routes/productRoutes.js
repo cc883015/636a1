a8b143ba21779e51ff9fd1a4f2b6ba694177be9e
cc883015 <1044940343@qq.com>
2025-04-15 20:14:20 +1000

    delete description

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -19,26 +19,3 @@ router.put('/:id', authMiddleware, updateProduct);
 router.delete('/:id', authMiddleware, deleteProduct);
 
 module.exports = router;
-// const express = require('express');
-// const { protect, isAdmin } = require('../middleware/authMiddleware');
-
-// const {
-//   getAllProducts,
-//   getProductById,
-//   createProduct,
-//   updateProduct,
-//   deleteProduct,
-// } = require('../controllers/productController');
-
-// const router = express.Router();
-
-// // 公共路由 - 所有人可访问
-// router.get('/', getAllProducts);
-// router.get('/:id', getProductById);
-
-// // 管理员路由 
-// router.post('/', protect, isAdmin, createProduct);
-// router.put('/:id', protect, isAdmin, updateProduct);
-// router.delete('/:id', protect, isAdmin, deleteProduct);
-
-// module.exports = router;
